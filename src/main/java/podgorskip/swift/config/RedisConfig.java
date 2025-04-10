297a51672ac39e74d5e185c77d6fbec68bf9c0e5
Patryk Podgórski <podgorski.p@interia.eu>
2025-04-10 07:15:16 +0200

    Adds Redis configuration

---
+++ src/main/java/podgorskip/swift/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package podgorskip.swift.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import podgorskip.swift.model.entities.SwiftCode;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, SwiftCode> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, SwiftCode> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(SwiftCode.class));
+        return template;
+    }
+}
+
