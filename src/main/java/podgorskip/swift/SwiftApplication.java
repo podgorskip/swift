109ff9ba05cde4640dcdb641cc4a5fe37a1f4793
Patryk Podgórski <podgorski.p@interia.eu>
2025-04-10 07:13:57 +0200

    Initial commit

---
+++ src/main/java/podgorskip/swift/SwiftApplication.java
@@ -0,0 +1,13 @@
+package podgorskip.swift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftApplication.class, args);
+	}
+
+}
